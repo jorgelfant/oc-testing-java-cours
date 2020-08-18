9b1ecb12a18cc2c2f57a4b6bb637471f4a93c0d0
jorgel_fant <jorgel_fant@yahoo.com>
2020-08-18 14:47:29 +0200

    Objectif 1: creer methode pour initialiser instance de Calculator avant l'execution des tests

--- src/test/java/com/openclassrooms/testing/CalculatorTest.java
+++ src/test/java/com/openclassrooms/testing/CalculatorTest.java
@@ -2,22 +2,50 @@ package com.openclassrooms.testing;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.Instant;
+
 class CalculatorTest {
+    //variables de l'instance en cours du calculateur et une variable de temps du début des tests
+    private Calculator calculatorunderTest;
+    private static Instant calculatorUnderTest;
 
-	@Test
-	void testAddTwoPositiveNumbers() {
-		// Arrange
-		int a = 2;
-		int b = 3;
-		Calculator calculator = new Calculator();
+    // objectif 1: initialiser une instance du calculateur avant chaque test, créons une méthode pour initialiser
+    // le calculateur (et ajouter un message à la console)
+    @BeforeEach // executer cette méthode avant chaque test
+    public void initCalculator() {
+        System.out.println("Appel avant chaque test");
+        calculatorunderTest = new Calculator();
+    }
+    // Il faut à présent supprimer les initialisations de Calculator dans les méthodes de tests
+    // pour utiliser l'instance calculatorUnderTest
 
-		// Act
-		int somme = calculator.add(a, b);
+    @Test
+    public void testAddTwoPositiveNumbers() {
+        // Arrange
+        int a = 2;
+        int b = 3;
 
-		// Assert
-		assertEquals(5, somme);
-	}
+        // Act
+        int somme = calculatorunderTest.add(a, b);
+
+        // Assert
+        assertEquals(5, somme);
+    }
+
+    @Test
+    public void multiply_shouldReturnTheProduct_ofTwoIntegers() {
+        // Arrange
+        int a = 42;
+        int b = 11;
+
+        // Act
+        int produit = calculatorunderTest.multiply(a, b);
+
+        // Assert
+        assertEquals(462, produit);
+    }
 
 }
