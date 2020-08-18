31bd9b559151ca46f75455eff6df8eeaa1fdbf32
jorgel_fant <jorgel_fant@yahoo.com>
2020-08-18 16:57:15 +0200

    done

--- src/test/java/com/openclassrooms/testing/CalculatorTest.java
+++ src/test/java/com/openclassrooms/testing/CalculatorTest.java
@@ -2,18 +2,19 @@ package com.openclassrooms.testing;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.time.Instant;
 
 class CalculatorTest {
     //variables de l'instance en cours du calculateur et une variable de temps du début des tests
     private Calculator calculatorunderTest;
-    private static Instant calculatorUnderTest;
+    private static Instant startedAt;
 
+    // =================================================================================================================
     // objectif 1: initialiser une instance du calculateur avant chaque test, créons une méthode pour initialiser
     // le calculateur (et ajouter un message à la console)
+    // =================================================================================================================
     @BeforeEach // executer cette méthode avant chaque test
     public void initCalculator() {
         System.out.println("Appel avant chaque test");
@@ -46,6 +47,34 @@ class CalculatorTest {
 
         // Assert
         assertEquals(462, produit);
+    }
+
+    // =================================================================================================================
+    // objectif 2 : après chaque test, mettre la valeur du calculateur à null), nous allons utiliser
+    // l'annotation @AfterEach
+    // Mettre la variable calculatorUnderTest à null n'a pas de réelle utilité ici. C'est juste pour montrer
+    // l'utilisation de cette annotation.
+    // =================================================================================================================
+    @AfterEach
+    public void undefCalculator() {
+        System.out.println("Appel après chaque test");
+        calculatorunderTest = null;
+    }
+    // =================================================================================================================
+    // Enfin, pour l'objectif 3 (mesurer le temps de traitement de l'ensemble des tests de CalculatorTest),
+    // nous allons créer des méthodes statiques annotées avec @BeforeAll et @AfterAll :
+    // =================================================================================================================
+
+    @BeforeAll
+    static public void initStartingTime() {
+        System.out.println("Appel avant tous les tests");
+        startedAt = Instant.now();
+    }
+
+    @AfterAll
+    static public void showTestDuration() {
+        System.out.println("Appel après tous les tests");
+        Instant endedAt = Instant.now();
     }
 
 }
